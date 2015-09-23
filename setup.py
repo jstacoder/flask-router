28324a31fe6ad116d9f26b6064d0913d7fbf8866
kyle roux <kyle@level2designs.com>
2015-09-23 11:26:59 -0700

    prep for upload

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(
+    version='0.0.1',
+    author='jstacoder',
     name='flask-router',
     packages=['flask_router'],
+    url='https://github.com/jstacoder/flask-router.git',
 )
